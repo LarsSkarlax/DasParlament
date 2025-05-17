1fdc52e5b16d2c0559b891b87038b676b7d9eceb
lrskeller <lars.keller@skarlax.de>
2025-05-17 10:35:44 +0200

    initial project commit

---
+++ app/src/main/java/com/lrs/dasparlament/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.lrs.dasparlament.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
