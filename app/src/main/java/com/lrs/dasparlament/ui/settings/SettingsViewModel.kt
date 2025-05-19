7a556a8382b023ff8243ff2b7f5eedf35a048420
lrskeller <lars.keller@skarlax.de>
2025-05-19 23:03:40 +0200

    settings fragment now working

--- app/src/main/java/com/lrs/dasparlament/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/lrs/dasparlament/ui/settings/SettingsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "There are currently no settings. So just enjoy simplicity :)"
     }
     val text: LiveData<String> = _text
 }
